ee11be573d40889c9c962a48546716d59b97f43a
Richard Shade <rshade@rightscale.com>
2015-04-02 16:41:19 -0500

    adding libxml2

---
+++ attributes/default.rb
@@ -0,0 +1,2 @@
+node.default['libxml2']['compile_time'] = false
+node.default['libxml2']['install_devel'] = true
--- recipes/default.rb
+++ recipes/default.rb
@@ -16,6 +16,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+include_recipe "libxml2"
+
 y=bash "yum install" do
     code <<-EOF
       yum groupinstall "Development Tools" -y
