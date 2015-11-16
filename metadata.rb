9482f1eda6ce73f5546f57a3e2d2f7200d7ad929
Curt Wilhelm <curt@rightscale.com>
2015-11-16 14:36:47 -0700

    use nokogiri instead of build-essential and libxml2 to build fog.

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description      'Installs/Configures fog'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
 
-depends "build-essential"
-depends "libxml2"
+
+depends "nokogiri"
 
 recipe "fog::default", "installs fog"
